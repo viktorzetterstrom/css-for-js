b1ae2ed00b22683a1b758e0df3a1c9805ab27e54
Viktor Zetterström <viktor.zetterstrom@sanalabs.com>
2022-08-10 16:31:24 +0200

    character-creator

---
+++ character-creator-main/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Background from "./components/Background";
+
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Background />
+      <CharacterEditor />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ character-creator-main/src/components/Background/Background.js
@@ -0,0 +1,7 @@
+import styles from './Background.module.css';
+
+function Background() {
+  return <div className={styles.background}></div>;
+}
+
+export default Background;
